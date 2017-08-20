1b7cab3297f4ce42278161486699626e51301baa
Yair Shkedi <yairshkedi@Yairs-MacBook-Pro.local>
2017-08-20 14:35:56 +0300

    change the scope to be scopr test

--- src/test/java/eq/QuadraticFormolaTest.java
+++ src/test/java/eq/QuadraticFormolaTest.java
@@ -4,6 +4,7 @@ import eq.QuadraticEquationCreator;
 import eq.QuadraticFormola;
 import eq.ResultDao;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -24,33 +25,32 @@ public class QuadraticFormolaTest {
     @Mock
     QuadraticEquationCreator quadraticEquationCreator;
 
+    @Before
+    public void init() {
+        qes.resultDao = resultDao;
+        qes.quadraticEquationCreator = quadraticEquationCreator;
+    }
+
     @Test
     //x^2+5x+4
     public void shouldReturnMinusOneAndMinusFour() {
-        qes.resultDao = resultDao;
-        qes.quadraticEquationCreator = quadraticEquationCreator;
-
         int[] eq = new int[]{1,5,4};
 
         when(resultDao.getResult(any(QuadraticEquation.class))).thenReturn(null);
         when(quadraticEquationCreator.create(eq)).thenReturn(new QuadraticEquation(1,5,4));
         Pair result = qes.solve(eq);
-        Assert.assertEquals(result, new Pair(-4,-1));
-
+        Assert.assertEquals(result, new Pair(-1,-4));
     }
 
     @Test
     //x^2+5x+4
     public void shouldReturnMinusOneAndMinusFourWhenResultExistInDB() {
-        qes.resultDao = resultDao;
-        qes.quadraticEquationCreator = quadraticEquationCreator;
-
         int[] eq = new int[]{1,5,4};
 
         when(resultDao.getResult(any(QuadraticEquation.class))).thenReturn(new Pair(-1,-4));
         when(quadraticEquationCreator.create(eq)).thenReturn(new QuadraticEquation(1,5,4));
         Pair result = qes.solve(eq);
-        Assert.assertEquals(result, new Pair(-4,-1));
+        Assert.assertEquals(result, new Pair(-1,-4));
 
     }
 
@@ -58,9 +58,6 @@ public class QuadraticFormolaTest {
     @Test
     //x^2+5x+4
     public void shouldReturnNullWhenNoSoultion() {
-        qes.resultDao = resultDao;
-        qes.quadraticEquationCreator = quadraticEquationCreator;
-
         int[] eq = new int[]{1,1,1};
 
         when(resultDao.getResult(any(QuadraticEquation.class))).thenReturn(null);
--- src/test/java/eq/TrinumFormolaTest.java
+++ src/test/java/eq/TrinumFormolaTest.java
@@ -1,6 +1,7 @@
 package eq;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -20,50 +21,42 @@ public class TrinumFormolaTest {
     @Mock
     QuadraticEquationCreator quadraticEquationCreator;
 
+    @Before
+    public void init() {
+        qes.resultDao = resultDao;
+        qes.quadraticEquationCreator = quadraticEquationCreator;
+    }
+
     @Test
     //x^2+5x+6
     public void shouldReturnMinusTwoAndMinusThree() {
-        qes.resultDao = resultDao;
-        qes.quadraticEquationCreator = quadraticEquationCreator;
-
         int[] eq = new int[]{1,5,6};
-
         when(resultDao.getResult(any(QuadraticEquation.class))).thenReturn(null);
         when(quadraticEquationCreator.create(eq)).thenReturn(new QuadraticEquation(1,5,6));
         Pair result = qes.solve(eq);
         Assert.assertEquals(result, new Pair(-2,-3));
-
     }
 
     @Test
     //x^2+5x+6
     public void shouldReturnMinusTwoAndMinusThreeWhenResultExistInDB() {
-        qes.resultDao = resultDao;
-        qes.quadraticEquationCreator = quadraticEquationCreator;
-
         int[] eq = new int[]{1,5,6};
-
         when(resultDao.getResult(any(QuadraticEquation.class))).thenReturn(new Pair(-2,-3));
         when(quadraticEquationCreator.create(eq)).thenReturn(new QuadraticEquation(1,5,6));
         Pair result = qes.solve(eq);
         Assert.assertEquals(result, new Pair(-2,-3));
-
     }
 
 
     @Test
     //x^2+5x+4
     public void shouldReturnNullWhenNoSoultion() {
-        qes.resultDao = resultDao;
-        qes.quadraticEquationCreator = quadraticEquationCreator;
-
         int[] eq = new int[]{1,1,1};
 
         when(resultDao.getResult(any(QuadraticEquation.class))).thenReturn(null);
         when(quadraticEquationCreator.create(eq)).thenReturn(new QuadraticEquation(1,1,1));
         Pair result = qes.solve(eq);
         Assert.assertNull(result);
-
     }
 
 }
